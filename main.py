155886a4c872e3d19641f70821b23ecb040f7930
Sirojiddin_Samadov <78139080+believer07asn@users.noreply.github.com>
2023-05-31 08:07:36 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!-- <html lang="en">
+    <head>
+        <title>Portfolio</title>
+        <!-- <link rel="stylesheet" href="./css/main.css"> -->
+        <style>
+            #blog-section{
+                background-color: red;
+                height: 100px;
+            }
+        </style>
+    </head>
+    <body>
+        <!--Header section-->
+        <header class="main-header">
+            <nav class="main-navigation">
+                <ul class="main-navigation__items">
+                    <li class="main-navigation__item"><a href="#">Asosiy</a></li>
+                    <li class="main-navigation__item"><a href="#">Men haqimda</a></li>
+                    <li class="main-navigation__item"><a href="#">Xizmatlar</a></li>
+                    <li class="main-navigation__item"><a href="#">Mijozlar</a></li>
+                    <li class="main-navigation__item"><a href="#">Portfolio</a></li>
+                    <li class="main-navigation__item"><a href="#">Blog</a></li>
+                    <li class="main-navigation__item"><a href="#">Sheriklar</a></li>
+                </ul>
+            </nav>
+        </header>
+        <!--/Header section-->
+
+        <!--Main section-->
+        <section id="main-section">
+            <h1>Asosiy qism</h1>
+        </section>
+        <!--/Main section-->
+
+        <!--About me section-->
+        <section id="about-me">
+            <h1>About me</h1>
+        </section>
+        <!--/About me section-->
+
+        <!--Service section-->
+        <section id="service">
+            <h1>Xizmatlar</h1>
+        </section>
+        <!--/Service section-->
+
+        <!--Clients section-->
+        <section id="client-section">
+            <h1>Mijozlar</h1>
+        </section>
+        <!--/Clients section-->
+
+        <!--Portfolio section-->
+        <section id="portfolio-section">
+            <h1>Portfolio</h1>
+        </section>
+        <!--/Portfolio section-->
+
+        <!--Blog section-->
+        <section id="blog-section">
+            <h1>Blog</h1>
+        </section>
+        <!--/Blog section-->
+
+        <!--Partners section-->
+        <section id="partner-section">
+            <h1>Sheriklar</h1>
+        </section>
+        <!--/Partners section-->
+
+        <!--Footere section-->
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Asosiy</a></li>
+                <li><a href="#">Men haqimda</a></li>
+                <li><a href="#">Xizmatlar</a></li>
+                <li><a href="#">Mijozlar</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Sheriklar</a></li>
+            </ul>
+        </nav>
+        <!--/Footere section-->
+</html> -->
